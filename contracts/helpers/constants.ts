b36fe7a2f644528bde4f9110767d479997c58388
Kangiding <hadiraihanyt1@gmail.com>
2026-01-15 13:14:23 +0700

    Add files via upload

---
+++ contracts/helpers/constants.ts
@@ -0,0 +1,3 @@
+export const RPC_URL = process.env.RPC_URL || "https://api.avax-test.network/ext/bc/C/rpc";
+export const USER_PRIVATE_KEY = process.env.PRIVATE_KEY || "0x90b1a7d8dc7d8376843179dabf362715ab055fa8d2df03b7225fb0dda36e7b6f";
+export const ETHERSCAN_API = process.env.ETHERSCAN_API || "";
